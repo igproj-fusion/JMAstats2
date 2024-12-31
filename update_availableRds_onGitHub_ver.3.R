88c54501ac2825016c21c3f205e484b9deafab98
igproj-fusion <myajima0202@gmail.com>
2025-01-01 06:55:15 +0900

    updated 2025/1/1

---
+++ update_availableRds_onGitHub_ver.3.R
@@ -0,0 +1,90 @@
+######################################################
+#
+# 月が変わって更新するときは、
+#
+# local Diskの"C:\Users\myaji\GitHub\JMAstats2\data\rds_update"
+# このフォルダの既存rdsファイルは削除される。
+# 先月分のデータが追加されたrdsファイルはここに保存される。
+#
+# 更新が終了したら全データを確認した後、問題が無ければ
+# GitHub/JMAstats2とsyncさせる。
+#
+######################################################
+
+
+pacman::p_load(
+  jmastats,
+  units,
+  tidyverse,
+  janitor,
+  here,
+  tools,
+  stringi,
+  bazar,
+  supportR)
+
+
+LOCAL_repo = "data/rds_update"
+files <- dir(here(LOCAL_repo))
+if (length(files) != 0) {
+  file.remove(here(LOCAL_repo, files))
+}
+
+
+GITHUB.raw = "https://raw.githubusercontent.com/igproj-fusion/"
+GITHUB.repo = "JMAstats2/main/data/rds_update/"
+
+
+Area.li <- read.csv(paste0(GITHUB.raw, "JMAstats/main/Area_list.csv"))
+
+
+RDS.github <- github_ls(repo = "https://github.com/igproj-fusion/JMAstats2", 
+                        recursive = TRUE, quiet = FALSE) |> 
+  filter(path == "./data/rds_update") |> 
+  mutate(name = paste0(GITHUB.raw, GITHUB.repo, name)) |> 
+  pull(name)
+
+YearMonth <- substr(RDS.github[1], 
+       str_length(RDS.github[1]) - 9, 
+       str_length(RDS.github[1]) - 4)
+
+Year <- as.numeric(substr(YearMonth, 1, 4))                      
+Month <- as.numeric(substr(YearMonth, 5, 6))
+
+if(Month == 12){
+  Year <- Year + 1
+  Month <- 1
+} else {
+  Month <- Month + 1
+}
+
+
+for(i in 1:length(RDS.github)) {
+  
+  df.org <- readRDS(url(RDS.github[i], method = "libcurl"))
+  
+  BLOCK <- df.org |> 
+    select(block_no) |> 
+    distinct(block_no, .keep_all = TRUE) |> 
+    pull(block_no)
+  
+  df <- set_names(BLOCK) |> 
+    map(\(BLOCK_NO) 
+        jma_collect(item = "daily",
+                    block_no = BLOCK_NO,
+                    year = Year, 
+                    month = Month, 
+                    cache = FALSE,
+                    quiet = TRUE) |> 
+          mutate(block_no = BLOCK_NO) |>
+          mutate(PREFECTURE = Area.li$Area_en[i])) |> 
+    bind_rows()
+  
+  Rds.name <- paste0("JMA_",  Area.li$Area_en[i],
+                     "_", Year, Month, ".rds")
+  saveRDS(bind_rows(df.org, df), 
+          file = here(LOCAL_repo, Rds.name))
+}  
+
+
+
